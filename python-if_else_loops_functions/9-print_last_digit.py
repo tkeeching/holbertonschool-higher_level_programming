2967e574df2b299b6ebc353bc9447ef81c7fbf3c
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 10:07:59 +1100

    Fixed test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    d = number % 10 if number > 0 else abs(number) % 10 * -1
-    print("{0}".format(d))
+    d = number % 10 if number > 0 else abs(number) % 10
+    print("{0}".format(d), end="")
+    return d
