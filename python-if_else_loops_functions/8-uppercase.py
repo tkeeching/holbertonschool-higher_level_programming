45573dbc5351a7c462a40f36ed085e6084d4f49d
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 09:54:28 +1100

    Tp uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in range(len(str)):
+        char = str[i]
+        if ((ord(char) >= 97) and (ord(char) <= 123)):
+            result += chr(ord(char) - 32)
+        else:     
+            result += char
+    print(result)
