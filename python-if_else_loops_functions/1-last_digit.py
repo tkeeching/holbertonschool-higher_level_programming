e96301914bee02047b6e7756c5baf766a9efc0b9
Kee Tee <keetee.ching@axa.com.sg>
2023-10-31 17:04:55 +1100

    Removed logs

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("number ->", number)
-print("number % 10 ->", number % 10)
-print("abs(number) % 10 ->", abs(number) % 10)
 if (number % 10) == 0:
     print(f"Last digit of {number} is {number % 10} and is 0")
 elif (abs(number) % 10) > 5 and number > 0:
