5be624aa4c8759e95f60bf8c052809b5b4c86aee
Kee Tee <keetee.ching@axa.com.sg>
2023-10-31 20:20:50 +1100

    Conditional

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(0,100):
+for i in range(0, 100):
     num = str(i).zfill(2)
-    print("{0}".format(num), end=", ")
+    if i == 99:
+        print("{0}".format(num), end="")
+    else:
+        print("{0}".format(num), end=", ")
