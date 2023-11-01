803179d33564c2bc5bd2633d67a5229df92f932a
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 15:31:24 +1100

    Test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     argument_length = len(argv)
-    print("{0} arguments".format(argument_length -1))
+    if (argument_length - 1 == 1):
+        print("{0} argument".format(argument_length -1))
+    else:
+        print("{0} arguments".format(argument_length -1))
 
     for i in range(1, argument_length):
         print("{0}: {1}".format(i, argv[i]))
