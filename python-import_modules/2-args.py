8280c0a1e1add89977cf6c0f641f43da830b1135
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 15:33:36 +1100

    Fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 from sys import argv
 
 if __name__ == "__main__":
-    argument_length = len(argv)
-    if (argument_length - 1 == 1):
-        print("{0} argument".format(argument_length -1))
+    argument_length = len(argv) - 1
+    if (argument_length == 0):
+        print("{0} arguments.".format(argument_length))
+    elif (argument_length == 1):
+        print("{0} argument:".format(argument_length))
     else:
-        print("{0} arguments".format(argument_length -1))
+        print("{0} arguments:".format(argument_length))
 
-    for i in range(1, argument_length):
+    for i in range(1, argument_length + 1):
         print("{0}: {1}".format(i, argv[i]))
