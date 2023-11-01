e22004dc528d909c492d7362b38ba5d152826e47
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 15:12:08 +1100

    Indent

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 from add_0 import add
 
 if __name__ == "__main__":
-  a = 1
-  b = 2
-  c = add(a, b)
-  print("{0} + {1} = {2}".format(a, b, c))
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{0} + {1} = {2}".format(a, b, c))
