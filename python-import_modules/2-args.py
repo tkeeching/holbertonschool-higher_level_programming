0ea40820cebc128b1a8354ca6d88237203e4bd8f
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 15:29:16 +1100

    Guard condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 
-argument_length = len(argv)
-print("{0} arguments".format(argument_length -1))
+if __name__ == "__main__":
+    argument_length = len(argv)
+    print("{0} arguments".format(argument_length -1))
 
-for i in range(1, argument_length):
-    print("{0}: {1}".format(i, argv[i]))
+    for i in range(1, argument_length):
+        print("{0}: {1}".format(i, argv[i]))
