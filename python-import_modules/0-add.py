cd42cc515f4b6e47148d52fc4825b409b69be0f5
Kee Tee <keetee.ching@axa.com.sg>
2023-11-01 15:06:34 +1100

    Import guard

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,9 +2,7 @@
 import add_0
 
 if __name__ == "__add__":
-    add()
-
-a = 1
-b = 2
-c = add_0.add(a, b)
-print("{0} + {1} = {2}".format(a, b, c))
+  a = 1
+  b = 2
+  c = add_0.add(a, b)
+  print("{0} + {1} = {2}".format(a, b, c))
