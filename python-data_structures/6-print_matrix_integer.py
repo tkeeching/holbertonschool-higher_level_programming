4c33cc0d914d339e9c2d44a4b2da3b3c715bf472
Kee Tee <keetee.ching@axa.com.sg>
2023-11-05 16:24:01 +1100

    Test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,28 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print("")
+            if (j == len(matrix[i]) - 1):
+                print("{:d}".format(matrix[i][j]), end="")
+            else:
+                print("{:d}".format(matrix[i][j]), end=" ")
+        print("x")
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print_matrix_integer(matrix)
+# print("--")
+# print_matrix_integer()
+
+# matrix2 = [
+#     [1],
+#     [2],
+#     [3]
+# ]
+
+# print_matrix_integer(matrix2)
+# print("--")
+# print_matrix_integer()
