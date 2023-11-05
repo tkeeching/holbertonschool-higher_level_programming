f35b0cc0a63bf5af671243b2cbdfbcfeeac85d85
Kee Tee <keetee.ching@axa.com.sg>
2023-11-05 12:34:17 +1100

    Who are you

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 import hidden_4
 
 if __name__ == "__main__":
-    print(hidden_4)
+    keys = hidden_4.__dir__()
+    print(keys)
+    for i in range(0, len(keys)):
+        if (keys[i][0] != '_'):
+            print(keys[i])
