a76292dd46c9d1a2af3997e4a0df14ebbcdef915
Kee Tee <keetee.ching@axa.com.sg>
2023-11-07 10:44:59 +1100

    Tuples addition

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = []
+    for i in range(2):
+        if (len(tuple_a) <= i):
+            result.append(0 + tuple_b[i])
+        elif (len(tuple_b) <= i):
+            result.append(tuple_a[i] + 0)
+        else:
+            result.append(tuple_a[i] + tuple_b[i])
+    return tuple(result)
+
+# Tests
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+tuple_c = (2, 4)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+print(add_tuple((), tuple_c))
