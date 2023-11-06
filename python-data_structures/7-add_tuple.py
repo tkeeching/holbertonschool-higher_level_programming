13ff377c3bea90f2eda514a22cd4b5f7e4cf180c
Kee Tee <keetee.ching@axa.com.sg>
2023-11-07 10:48:40 +1100

    Oops

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -10,13 +10,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
             result.append(tuple_a[i] + tuple_b[i])
     return tuple(result)
 
-# Tests
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-tuple_c = (2, 4)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
-print(add_tuple((), tuple_c))
+# Tests
+# tuple_a = (1, 89)
+# tuple_b = (88, 11)
+# tuple_c = (2, 4)
+# new_tuple = add_tuple(tuple_a, tuple_b)
+# print(new_tuple)
+
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
+# print(add_tuple((), tuple_c))
