d81115845fee80d2b504f2dadfc637f9b1d92bcd
Kee Tee <keetee.ching@axa.com.sg>
2023-11-07 12:27:57 +1100

    Comment tests

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -8,8 +8,8 @@ def delete_at(my_list=[], idx=0):
 
 
 # Tests
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+# my_list = [1, 2, 3, 4, 5]
+# idx = 3
+# new_list = delete_at(my_list, idx)
+# print(new_list)
+# print(my_list)
