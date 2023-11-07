27d175ec8644082d01df9476eda5cd20214564ba
Kee Tee <keetee.ching@axa.com.sg>
2023-11-07 12:03:19 +1100

    Oops

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -11,6 +11,6 @@ def max_integer(my_list=[]):
 
 
 # Tests
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+# my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+# max_value = max_integer(my_list)
+# print("Max: {}".format(max_value))
