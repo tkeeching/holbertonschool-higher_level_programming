4f15c224a9291702da12b4543060df90c85615da
Kee Tee <keetee.ching@axa.com.sg>
2023-11-07 12:08:04 +1100

    Fix test

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return None
-    
-    prev_int = 0
+
+    prev_int = my_list[0] or 0
     for i in range(len(my_list)):
         if (my_list[i] > prev_int):
             prev_int = my_list[i]
@@ -12,5 +12,7 @@ def max_integer(my_list=[]):
 
 # Tests
 # my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+# my_list = [-1, -2, -3, -4]
+# my_list = []
 # max_value = max_integer(my_list)
 # print("Max: {}".format(max_value))
