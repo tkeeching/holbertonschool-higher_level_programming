143d0e76355a103d062a6df9c6349f1e8c91e0eb
Kee Tee <keetee.ching@axa.com.sg>
2023-11-08 16:23:37 +1100

    Oops

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -9,7 +9,7 @@ def common_elements(set_1, set_2):
 
 
 # Tests
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# c_set = common_elements(set_1, set_2)
+# print(sorted(list(c_set)))
