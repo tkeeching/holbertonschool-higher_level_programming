6c8c065a25d828c2f4f3e471232f1dfee5a3fd49
Kee Tee <keetee.ching@axa.com.sg>
2023-11-08 17:03:55 +1100

    Only differents

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_list = set_1.union(set_2)
+    common_list = []
+
+    for i in set_1:
+        for j in set_2:
+            if (i == j):
+                common_list.append(i)
+
+    for k in common_list:
+        new_list.remove(k)
+          
+    return new_list
+
+
+# Tests
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# od_set = only_diff_elements(set_1, set_2)
+# print(sorted(list(od_set)))
