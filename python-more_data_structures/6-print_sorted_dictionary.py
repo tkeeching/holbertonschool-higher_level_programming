6f5b5668e3f81c55b24b55a5c3f3dec88bbbf256
Kee Tee <keetee.ching@axa.com.sg>
2023-11-09 09:07:31 +1100

    Print sorted dictionary

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary):
+        print("{0}: {1}".format(i, a_dictionary[i]))
+
+
+# Tests
+# a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+# print_sorted_dictionary(a_dictionary)
