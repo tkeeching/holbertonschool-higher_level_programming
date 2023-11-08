b986e58546d0159c340c79ba364549458d54c69b
Kee Tee <keetee.ching@axa.com.sg>
2023-11-09 09:37:04 +1100

    Lint

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     i = 0
-    while i < len(a_dictionary):
+    while i <= len(a_dictionary):
         a_dictionary[key] = value
         i += 1
     return a_dictionary
 
 
 # Tests
-# print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+# print_sorted_dictionary = __import__(
+#     '6-print_sorted_dictionary').print_sorted_dictionary
 
 # a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 # new_dict = update_dictionary(a_dictionary, 'language', "Python")
@@ -23,3 +24,9 @@ def update_dictionary(a_dictionary, key, value):
 # print_sorted_dictionary(new_dict)
 # print("--")
 # print_sorted_dictionary(a_dictionary)
+
+# b_dictionary = {}
+# new_dict = update_dictionary(b_dictionary, 'a', "a")
+# print_sorted_dictionary(new_dict)
+# print("xx")
+# print_sorted_dictionary(b_dictionary)
