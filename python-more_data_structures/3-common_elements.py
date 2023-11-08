cb62bfb2dea4229a5f397ae8969ff0ed2a54bedd
Kee Tee <keetee.ching@axa.com.sg>
2023-11-08 16:22:52 +1100

    Present in both

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    new_list = []
+    for i in set_1:
+        for j in set_2:
+            if (i == j):
+                new_list.append(i)
+    return new_list
+
+
+# Tests
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
