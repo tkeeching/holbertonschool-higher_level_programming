2436aee638a219eebaddc626eec0566ac6b0a130
Kee Tee <keetee.ching@axa.com.sg>
2023-11-09 10:03:58 +1100

    Lint

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -9,7 +9,8 @@ def simple_delete(a_dictionary, key=""):
 # print_sorted_dictionary = \
 #     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-# a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+# a_dictionary = {
+#     'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
 # new_dict = simple_delete(a_dictionary, 'track')
 # print_sorted_dictionary(a_dictionary)
 # print("--")
