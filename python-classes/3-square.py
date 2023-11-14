f5b8988d6af898eceabec651554b4281f904a2b0
Kee Tee <keetee.ching@axa.com.sg>
2023-11-15 10:50:51 +1100

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Represents a Square with size"""
+
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+
+# Tests
+# my_square_1 = Square(3)
+# print("Area: {}".format(my_square_1.area()))
+
+# try:
+#     print(my_square_1.size)
+# except Exception as e:
+#     print(e)
+
+# try:
+#     print(my_square_1.__size)
+# except Exception as e:
+#     print(e)
+
+# my_square_2 = Square(5)
+# print("Area: {}".format(my_square_2.area()))
