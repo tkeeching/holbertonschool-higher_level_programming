27fa867ee3fb229aa6929fd899c43d4591c270b0
Kee Tee <keetee.ching@axa.com.sg>
2023-11-14 16:42:58 +1100

    Oops

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -18,14 +18,14 @@ def list_division(my_list_1, my_list_2, list_length):
 
 
 # Tests
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+# my_l_1 = [10, 8, 4]
+# my_l_2 = [2, 4, 4]
+# result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+# print(result)
 
-print("--")
+# print("--")
 
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+# my_l_1 = [10, 8, 4, 4]
+# my_l_2 = [2, 0, "H", 2, 7]
+# result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+# print(result)
