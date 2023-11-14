b126e5d9440240b0c7eb91c248e7305a096f3475
Kee Tee <keetee.ching@axa.com.sg>
2023-11-15 10:08:38 +1100

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Represents a Square with size"""
+
+    def __init__(self, size):
+        self.__size = size
+
+
+# Tests
+# my_square = Square(3)
+# print(type(my_square))
+# print(my_square.__dict__)
+
+# try:
+#     print(my_square.size)
+# except Exception as e:
+#     print(e)
+
+# try:
+#     print(my_square.__size)
+# except Exception as e:
+#     print(e)
