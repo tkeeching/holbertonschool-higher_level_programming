bbdc04a566f764f1228a963e483ac16ddea5a788
Kee Tee <keetee.ching@axa.com.sg>
2023-11-14 13:00:11 +1100

    test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{0}".format(my_list[i]), end="")
         except IndexError:
             nb_print -= 1
+            print("")
             return nb_print
     print("")
     return nb_print
@@ -14,13 +15,13 @@ def safe_print_list(my_list=[], x=0):
 
 # Tests
 # my_list = [1, 2, 3, 4, 5]
-# my_list = [1, 2, 3, 4]
+my_list = [1, 2, 3, 4]
 
-# nb_print = safe_print_list(my_list, 2)
-# print("nb_print: {:d}".format(nb_print))
-# nb_print = safe_print_list(my_list, len(my_list))
-# print("nb_print: {:d}".format(nb_print))
-# nb_print = safe_print_list(my_list, len(my_list) + 2)
-# print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
 # nb_print = safe_print_list(my_list, 0)
 # print("nb_print: {:d}".format(nb_print))
