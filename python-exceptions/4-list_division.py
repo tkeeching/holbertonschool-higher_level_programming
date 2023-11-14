a886cb99817c6880f64b4f4b86294c1a7aa39b14
Kee Tee <keetee.ching@axa.com.sg>
2023-11-14 16:58:24 +1100

    test

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,8 +11,10 @@ def list_division(my_list_1, my_list_2, list_length):
             output = 0
         except TypeError:
             print("wrong type")
+            output = 0
         except IndexError:
             print("out of range")
+            output = 0
         finally:
             result.append(output)
 
@@ -36,5 +38,12 @@ def list_division(my_list_1, my_list_2, list_length):
 
 # my_l_1 = [10, 0, 4]
 # my_l_2 = [2, 4, 0]
+# result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+# print(result)
+
+# print("--")
+
+# my_l_1 = [10, 2]
+# my_l_2 = [2]
 # result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
 # print(result)
