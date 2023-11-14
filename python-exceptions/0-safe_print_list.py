0806853fc3bcb987838cffcdc04164b86de3eac9
Kee Tee <keetee.ching@axa.com.sg>
2023-11-14 12:58:20 +1100

    test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,23 +4,23 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         nb_print += 1
         try:
-            if (nb_print == x):
-                print("{0}".format(my_list[i]))
-            else:
-                print("{0}".format(my_list[i]), end="")
-        except:
+            print("{0}".format(my_list[i]), end="")
+        except IndexError:
             nb_print -= 1
-            print("")
             return nb_print
+    print("")
     return nb_print
 
 
 # Tests
 # my_list = [1, 2, 3, 4, 5]
+# my_list = [1, 2, 3, 4]
 
 # nb_print = safe_print_list(my_list, 2)
 # print("nb_print: {:d}".format(nb_print))
 # nb_print = safe_print_list(my_list, len(my_list))
 # print("nb_print: {:d}".format(nb_print))
 # nb_print = safe_print_list(my_list, len(my_list) + 2)
+# print("nb_print: {:d}".format(nb_print))
+# nb_print = safe_print_list(my_list, 0)
 # print("nb_print: {:d}".format(nb_print))
