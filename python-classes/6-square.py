c03fa8308e775c93cc951f35357340b84241e670
Kee Tee <keetee.ching@axa.com.sg>
2023-11-15 16:34:38 +1100

    Oops

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -36,7 +36,7 @@ class Square:
 
         if (len(value) != 2
                 or type(value[0]) is not int
-                or type(value[0]) is not int
+                or type(value[1]) is not int
                 or value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
