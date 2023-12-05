9cdff0419f25fd5072e80b9b303fbc9cb8228704
Kee Tee <tkeeching@gmail.com>
2023-12-06 10:13:18 +1100

    copy a list object

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def copy_list(a_list):
+    return list(a_list)
+
+
+# Tests
+# my_list = [1, 2, 3]
+# print(my_list)
+
+# new_list = copy_list(my_list)
+
+# print(my_list)
+# print(new_list)
+
+# print(new_list == my_list)
+# print(new_list is my_list)
