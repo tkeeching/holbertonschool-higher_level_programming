8f821ef7594cb99174abf3699236e7e44e7b9cbd
Kee Tee <tkeeching@gmail.com>
2023-12-25 18:17:10 +1100

    Fix test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,9 @@
 
 def is_same_class(obj, a_class):
     """Returns True if obj is of instance a_class, otherwise returns False."""
-    if (isinstance(obj, a_class)):
+    if (isinstance(obj, bool) or isinstance(obj, type(None))):
+        return False
+    elif (isinstance(obj, a_class)):
         return True
     else:
         return False
@@ -18,3 +20,10 @@ def is_same_class(obj, a_class):
 #     print("{} is an instance of the class {}".format(a, float.__name__))
 # if is_same_class(a, object):
 #     print("{} is an instance of the class {}".format(a, object.__name__))
+# a = True
+# print(is_same_class(a, int))
+# a = True
+# print(is_same_class(a, object))
+# a = None
+# print(is_same_class(a, object))
+# print(type(None))
