46b192c87ae56166b5e28f993cff67bc5bc18d2b
Kee Tee <tkeeching@gmail.com>
2024-01-13 16:30:21 +1100

    Geometry module

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry():
+    """Empty class."""
+
+    def __init__(self):
+        pass
+
+
+# Tests
+# bg = BaseGeometry()
+
+# print(bg)
+# print(dir(bg))
+# print(dir(BaseGeometry))
