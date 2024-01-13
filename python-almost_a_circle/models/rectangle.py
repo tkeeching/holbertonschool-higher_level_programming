b257f0396dfbdcf958b47cbcb6ae3d6b8523b167
Kee Tee <tkeeching@gmail.com>
2024-01-14 09:46:53 +1100

    Test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-from base import Base
+"""Base module"""
+
+
+class Base():
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if (id is not None):
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
 
 """Rectangle module"""
 
