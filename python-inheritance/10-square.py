a87759bcb2f1da2e48d7e1ba738bea5666a85009
Kee Tee <tkeeching@gmail.com>
2024-01-13 18:08:17 +1100

    Square #1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry():
+    """BaseGeometry class."""
+
+    def __init__(self):
+        pass
+
+    def integer_validator(self, name, value):
+        if (type(value) is not int):
+            raise TypeError("{0} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{0} must be greater than 0".format(name))
+
+
+"""Rectangle module"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        if self.integer_validator("width", width) is None:
+            self.__width = width
+        if self.integer_validator("height", height) is None:
+            self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __repr__(self):
+        """Internal representation of the rectangle"""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+"""Square module"""
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size):
+        if self.integer_validator("size", size) is None:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __repr__(self):
+        """Internal representation of the square"""
+
+        return f"[Rectangle] {self.__size}/{self.__size}"
+
+
+# Tests
+# s = Square(13)
+
+# print(s)
+# print(s.area())
