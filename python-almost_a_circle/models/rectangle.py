8691d10d12e7020f34720a9f10246669b74c5442
Kee Tee <tkeeching@gmail.com>
2024-01-14 14:53:35 +1100

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,51 +9,67 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self._width(width)
-        self._height(height)
-        self._x(x)
-        self._y(y)
+        self.__width(width)
+        self.__height(height)
+        self.__x(x)
+        self.__y(y)
 
-    def _width(self, width):
+    def __width(self, width):
         """width setter"""
 
-        self.__width = width
+        self.__width = self.validator("width", width)
 
     def width(self):
         """width getter"""
 
         return self.__width
 
-    def _height(self, height):
+    width = property(width, __width)
+
+    def __height(self, height):
         """height setter"""
 
-        self.__height = height
+        self.__height = self.validator("height", height)
 
     def height(self):
         """height getter"""
 
         return self.__height
 
-    def _x(self, x):
-        """x setter"""
+    height = property(height, __height)
 
-        self.__x = x
+    def __x(self, x):
+        """x setter"""
+        self.__x = self.validator("x", x)
 
     def x(self):
         """x getter"""
 
         return self.__x
 
-    def _y(self, y):
+    x = property(x, __x)
+
+    def __y(self, y):
         """y setter"""
 
-        self.__y = y
+        self.__y = self.validator("y", y)
 
     def y(self):
         """y getter"""
 
         return self.__y
 
+    y = property(y, __y)
+
+    def validator(self, name, value):
+        if (type(value) is not int):
+            raise TypeError("{} must be an integer".format(name))
+        elif ((name == "x" or name == "y") and value < 0):
+            raise ValueError("{} must be >= 0".format(name))
+        elif (name != "x" and name != "y" and value <= 0):
+            raise ValueError("{} must be > 0".format(name))
+        else:
+            return value
 
 # Tests
 # r1 = Rectangle(10, 2)
@@ -67,3 +83,26 @@ class Rectangle(Base):
 # print(r3.id)
 
 # print(issubclass(Rectangle, Base))
+
+# Task 3 Tests
+# try:
+#     Rectangle(10, "2")
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
+
+# try:
+#     r = Rectangle(10, 2)
+#     r.width = -10
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
+
+# try:
+#     r = Rectangle(10, 2)
+#     r.x = {}
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
+
+# try:
+#     Rectangle(10, 2, 3, -1)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
