861cec8db1e4301770e8746992bf65c046e90b85
Kee Tee <tkeeching@gmail.com>
2024-01-25 11:20:32 +1100

    Add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""read_file module"""
 
 def read_file(filename=""):
+    """Prints content of a file"""
+
     with open(filename, encoding="utf-8") as f:
         print(f.read())
  
