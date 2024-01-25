5e2a51a814966444b4465ff53185d940ca0deb14
Kee Tee <tkeeching@gmail.com>
2024-01-25 11:23:20 +1100

    Fix style

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """read_file module"""
 
+
 def read_file(filename=""):
     """Prints content of a file"""
 
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
- 
+
 # # # Tests
 # read_file("my_file_0.txt")
