7fa68beae2675a272946524f8f4d635f4ccf8f71
Kee Tee <keetee.ching@axa.com.sg>
2024-01-25 16:04:23 +1100

    Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""append_write module"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file and return number of characters added"""
+
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
+
+
+# Tests
+# nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+# print(nb_characters_added)
