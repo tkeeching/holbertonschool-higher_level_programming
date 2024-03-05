732e645fa88435f17594b3d8f4ce7319119fb820
Kee Tee <keetee.ching@axa.com.sg>
2024-03-05 14:26:48 +1100

    Fix styling

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Rectangle module"""
 
-from models.base import Base # for checker
-# from base import Base # for local
+from models.base import Base  # for checker
+# from base import Base  # for local
 
 
 class Rectangle(Base):
