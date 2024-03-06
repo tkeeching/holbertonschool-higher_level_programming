1b795bcdd01593a85adee11353e20ab062301d72
Kee Tee <keetee.ching@axa.com.sg>
2024-03-06 11:22:02 +1100

    Oops

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Square module"""
 
-# from models.rectangle import Rectangle  # for checker
-from rectangle import Rectangle  # for local
+from models.rectangle import Rectangle  # for checker
+# from rectangle import Rectangle  # for local
 
 
 class Square(Rectangle):
