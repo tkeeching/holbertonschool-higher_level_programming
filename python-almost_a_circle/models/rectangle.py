3718d6a2077a4957b5276d4ff35f17a81a0ff45f
Kee Tee <keetee.ching@axa.com.sg>
2024-03-06 11:34:10 +1100

    Rectangle instance to dictionary representation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -117,6 +117,9 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
+    def to_dictionary(self):
+        return self.__dict__
+
 # Tests
 # r1 = Rectangle(10, 2)
 # print(r1.id)
@@ -232,3 +235,16 @@ class Rectangle(Base):
 
 # r1.update(x=1, height=2, y=3, width=4)
 # print(r1)
+
+# Task 13
+# r1 = Rectangle(10, 2, 1, 9)
+# print(r1)
+# r1_dictionary = r1.to_dictionary()
+# print(r1_dictionary)
+# print(type(r1_dictionary))
+
+# r2 = Rectangle(1, 1)
+# print(r2)
+# r2.update(**r1_dictionary)
+# print(r2)
+# print(r1 == r2)
