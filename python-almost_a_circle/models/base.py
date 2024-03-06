2cf5086fdce0ab1b45d34a597400a5efe28e2a3e
Kee Tee <keetee.ching@axa.com.sg>
2024-03-06 12:09:50 +1100

    Fix styling

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 
 import json
 
+
 class Base():
     """Base class"""
     __nb_objects = 0
@@ -13,14 +14,14 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
 
-        if (list_dictionaries == None or len(list_dictionaries) == 0):
+        if (list_dictionaries is None or len(list_dictionaries) == 0):
             return "[]"
-        
+
         return json.dumps(list_dictionaries)
 
 
