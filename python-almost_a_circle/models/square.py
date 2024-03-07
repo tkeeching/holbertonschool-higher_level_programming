43030aa790c36530a6907178fc9039a6e5dd5dd0
Kee Tee <keetee.ching@axa.com.sg>
2024-03-07 16:05:12 +1100

    Tests

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -126,3 +126,20 @@ class Square(Rectangle):
 # Task 18
 # s1 = Square.create(**{'size': 2})
 # print(s1)
+    
+# Task 19
+# s1 = Square(5)
+# s2 = Square(7, 9, 1)
+# list_squares_input = [s1, s2]
+
+# Square.save_to_file(list_squares_input)
+
+# list_squares_output = Square.load_from_file()
+
+# for square in list_squares_input:
+#     print("[{}] {}".format(id(square), square))
+
+# print("---")
+
+# for square in list_squares_output:
+#     print("[{}] {}".format(id(square), square))
