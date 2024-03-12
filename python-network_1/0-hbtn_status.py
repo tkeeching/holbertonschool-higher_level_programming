97d39c3d064a6806a3b2fa407c1e68716d5f02a7
Kee Tee <keetee.ching@axa.com.sg>
2024-03-12 11:25:44 +1100

    Test

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,8 +2,26 @@
 
 import urllib.request
 
-req = urllib.request.Request(url='https://intranet.hbtn.io/status', headers={'User-Agent': 'Mozilla/5.0'})
+req = urllib.request.Request(
+    url='https://intranet.hbtn.io/status',
+    headers={'User-Agent': 'Mozilla/5.0'})
 
 with urllib.request.urlopen(req) as response:
     html = response.read()
-    print(html)
+    html_decoded = html.decode('utf8')
+
+    print('Body response:')
+    print('    - type: {}'.format(type(html)))
+
+    start_index = html_decoded.find(
+            '<pre style="word-wrap: break-word; white-space: pre-wrap;">')
+    + len('<pre style="word-wrap: break-word; white-space: pre-wrap;">')
+
+    end_index = html_decoded.find('</pre>', start_index)
+
+    if start_index != -1 and end_index != -1:
+        message = html_decoded[start_index:end_index]
+        print('    - content: {}'.format(message))
+        print('    - utf8 content: {}'.format(message))
+    else:
+        print("Message not found.")
