853f93377498ceeaa035d99babd11b048211fe21
Kee Tee <keetee.ching@axa.com.sg>
2024-03-12 16:34:07 +1100

    Test

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,16 +4,16 @@ import urllib.request
 import urllib.error
 import sys
 
-"""Takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id in the response header"""
-
-req = urllib.request.Request(
-    url=sys.argv[1],
-    headers={'User-Agent': 'Mozilla/5.0'})
-
 try:
+    req = urllib.request.Request(
+        url=sys.argv[1] if len(sys.argv) > 0 else '',
+        headers={'User-Agent': 'Mozilla/5.0'})
+
     with urllib.request.urlopen(req) as response:
         print(response.headers['X-Request-Id'])
+
+except IndexError as error:
+    print('IndexError:', error)
 
 except urllib.error.HTTPError as error:
     print(error)
