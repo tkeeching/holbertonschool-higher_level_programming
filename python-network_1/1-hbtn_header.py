76a83fba608e368233a1a585e6c22c21c690d441
Kee Tee <keetee.ching@axa.com.sg>
2024-03-12 16:27:23 +1100

    Document

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,6 +4,9 @@ import urllib.request
 import urllib.error
 import sys
 
+"""Takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id in the response header"""
+
 req = urllib.request.Request(
     url=sys.argv[1],
     headers={'User-Agent': 'Mozilla/5.0'})
