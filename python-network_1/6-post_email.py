8a645f4127ad6c0f14164ada3e5f4073dea3df44
Kee Tee <keetee.ching@axa.com.sg>
2024-03-13 12:24:43 +1100

    Test

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 try:
-    response = requests.post(sys.argv[1], json={'email': sys.argv[2]})
+    response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(response.text)
 except IndexError as error:
     print(error)
