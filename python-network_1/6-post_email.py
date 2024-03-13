adc9d8d86e865884968935c7ab1d42522275d291
Kee Tee <keetee.ching@axa.com.sg>
2024-03-13 12:20:14 +1100

    POST an email

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+try:
+    response = requests.get(sys.argv[1], json={'email': sys.argv[2]})
+    print(response.text)
+except IndexError as error:
+    print(error)
