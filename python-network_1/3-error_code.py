418a3468c63d60bf771fc126206acfd807f7f014
Kee Tee <keetee.ching@axa.com.sg>
2024-03-13 11:38:33 +1100

    Error code

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import urllib.request
+import urllib.error
+import urllib.parse
+import sys
+
+try:
+    req = urllib.request.Request(
+        url=sys.argv[1] if len(sys.argv) > 0 else '',
+        headers={'User-Agent': 'Mozilla/5.0'})
+
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(response)
+
+except IndexError as error:
+    print('IndexError:', error)
+
+except urllib.error.HTTPError as error:
+    print('Error code:', error)
