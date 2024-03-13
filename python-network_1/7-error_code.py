44a59e4627d5744dcf950319c8900c7e6429d7f9
Kee Tee <keetee.ching@axa.com.sg>
2024-03-13 12:36:49 +1100

    Error code

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+try:
+    response = requests.get(sys.argv[1])
+
+except IndexError as error:
+    print('IndexError:', error)
+
+except requests.HTTPError as error:
+    print('Error code:', error.getcode())
