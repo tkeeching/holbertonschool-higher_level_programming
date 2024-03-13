2fb78b157b0e82efdf6d57b8961336ed06eba8e7
tkeeching <tkeeching@gmail.com>
2024-03-13 15:30:05 +1100

    Test

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ import sys
 try:
     response = requests.post(
         'http://0.0.0.0:5000/search_user',
-        data={'q': '' if len(sys.argv) == 1 else sys.argv[1]})
+        data={'q': "" if len(sys.argv) == 1 else sys.argv[1]})
 
     json = response.json()
     if (len(json) == 0):
@@ -19,6 +19,3 @@ except IndexError as error:
 
 except ValueError as error:
     print('Not a valid JSON')
-
-except ConnectionRefusedError as error:
-    print('ConnectionRefusedError:', error)
