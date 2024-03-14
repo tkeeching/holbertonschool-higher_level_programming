c41157d3737074186aadc3a7dd577b9e9e9ae874
tkeeching <tkeeching@gmail.com>
2024-03-14 14:57:53 +1100

    First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Command to create a table called 'first_table' in a database in MySQL server.
+
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256)
+)
