f2cbc944f787287caff91b812c23a76f3ee57fe4
tkeeching <tkeeching@gmail.com>
2024-03-15 11:07:08 +1100

    Test

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
 -- Command to prints the following description of the table
 -- first_table from the database hbtn_0c_0 in MySQL server.
 
-DESC first_table;
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_table';
