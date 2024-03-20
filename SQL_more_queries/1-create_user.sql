e91c14d2f71fcd17e13bd826aec04135a1765e96
tkeeching <tkeeching@gmail.com>
2024-03-21 10:50:20 +1100

    Test

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -5,5 +5,5 @@
 -- The 'user_0d_1' password should be set to 'user_0d_1_pwd'
 -- Id the user 'user_0d_1' already exists, the script should not fail
 
-CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'user_0d_1'@'localhost';
