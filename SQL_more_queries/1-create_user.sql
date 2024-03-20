37b15c6409ec2a51e18d781ca1bd45a135981eb5
tkeeching <tkeeching@gmail.com>
2024-03-20 15:57:42 +1100

    Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Command to create MySQL server user 'user_0d_1'
+--
+-- Requirements:
+-- 'user_0d_1' should have all privileges
+-- The 'user_0d_1' password should be set to 'user_0d_1_pwd'
+-- Id the user 'user_0d_1' already exists, the script should not fail
+
+CREATE USER 'user_0d_1@localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
