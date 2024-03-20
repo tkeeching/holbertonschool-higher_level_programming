adae256a021aff0c535ae03560dc82adf2fb579f
tkeeching <tkeeching@gmail.com>
2024-03-20 11:28:03 +1100

    Average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- Command to compute the score average of all records
+-- in the table 'second_table'
+--
+-- Requirements:
+-- The result column name should be 'average'
+
+SELECT AVG(score) AS average FROM second_table;
