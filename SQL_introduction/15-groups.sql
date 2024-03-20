4bc7934febd3b6a3ab42e79998541e758c8e3d03
tkeeching <tkeeching@gmail.com>
2024-03-20 11:37:06 +1100

    Oops

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -7,4 +7,4 @@
 --   the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
 
-SELECT score COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
