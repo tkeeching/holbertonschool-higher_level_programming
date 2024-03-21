f3f0f56ada59e0f4a2c667954ada5b2766a211cf
tkeeching <tkeeching@gmail.com>
2024-03-21 15:57:04 +1100

    Always a name

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,12 @@
+-- Command to create the table 'force_name'
+--
+-- Requirements:
+-- 'force_name' description:
+--     'id' INT
+--     'name' VARCHAR(256) can’t be null
+-- If the table 'force_name' already exists, the script should not fail
+
+CREATE TABLE IF NOT EXISTS force_name {
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+}
