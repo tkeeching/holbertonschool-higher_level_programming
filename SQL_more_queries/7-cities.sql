314d2fe0b04337f00c169556042c70db95f170df
tkeeching <tkeeching@gmail.com>
2024-03-26 17:59:48 +1100

    Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,18 @@
+-- Command to create the database 'hbtn_0d_usa' and the table 'cities'
+--
+-- Requirements:
+-- 'cities' description:
+--     'id' INT unique, auto genereated, can't be null and is a primary key
+--     'stated_id' INT, can't be null and must be a FOREIGN KEY that references
+--            to 'idi' of the 'states' table
+--      'name' VARCHAR(256) can't be null
+-- If the databse 'hbtn_0d_usa' already exists, the script should not fail
+-- If the table 'cities' already exists, the script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  state_id INT,
+  FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+  name VARCHAR(256) NOT NULL
+);
