ed471482039dc136366bcc1a4476b5ccfc99db3c
tkeeching <tkeeching@gmail.com>
2024-03-26 17:11:31 +1100

    States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- Command to create the database 'hbtn_0d_usa' and the table 'states'
+--
+-- Requirements:
+-- 'states' description:
+--     'id' INT unique, auto genereated, can't be null and is a primary key
+--     'name' VARCHAR(256) can't be null
+-- If the databse 'hbtn_0d_usa' already exists, the script should not fail
+-- If the table 'states' already exists, the script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.unique_id (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
