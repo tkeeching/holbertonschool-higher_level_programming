f47565215fed183c979f6ea4aebf15e44cbda330
tkeeching <tkeeching@gmail.com>
2024-03-26 11:08:37 +1100

    Test

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -7,6 +7,6 @@
 -- If the table 'force_name' already exists, the script should not fail
 
 CREATE TABLE IF NOT EXISTS force_name {
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT,
   name VARCHAR(256) NOT NULL
 }
