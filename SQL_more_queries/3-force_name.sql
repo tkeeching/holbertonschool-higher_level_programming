a5b68dafce48607c1558fc46fd64522b902432e6
tkeeching <tkeeching@gmail.com>
2024-03-26 14:20:47 +1100

    Fix

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -6,7 +6,7 @@
 --     'name' VARCHAR(256) can’t be null
 -- If the table 'force_name' already exists, the script should not fail
 
-CREATE TABLE IF NOT EXISTS force_name {
+CREATE TABLE IF NOT EXISTS force_name (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(256) NOT NULL
-}
+);
