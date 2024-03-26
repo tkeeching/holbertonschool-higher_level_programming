4a9b6cf653e952ee5a9533a6c5a7ed9981522821
tkeeching <tkeeching@gmail.com>
2024-03-27 10:55:47 +1100

    Cities by States

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- Command to list all the cities in 'hbtn_0d_usa'
+--
+-- Requirements:
+--    Each record should display 'cities.id' - 'cities.name' - 'states.name'
+--    Results must be sorted in ascending order by 'cities.id'
+--    You are not allowed to use the 'JOIN' keyword
+
+SELECT id, name FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
