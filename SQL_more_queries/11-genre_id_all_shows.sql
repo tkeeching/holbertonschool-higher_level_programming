b0dcfbb18ab1ee385696501b38871cb86f5cdf1d
tkeeching <tkeeching@gmail.com>
2024-03-27 11:33:23 +1100

    Use alias

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -6,7 +6,7 @@
 --    If a show doesn’t have a genre, display 'NULL'
 --    You can use only one 'SELECT' statement
 
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL')
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, genre_id ASC;
