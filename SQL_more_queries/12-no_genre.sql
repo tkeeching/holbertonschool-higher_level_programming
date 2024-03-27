1227195aa89da1be6aa986e128a8508e54a8b7ff
tkeeching <tkeeching@gmail.com>
2024-03-27 14:09:34 +1100

    No genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,13 @@
+-- Command to list all shows contained in 'hbtn_0d_tvshows' without a genre linked
+--
+-- Requirements:
+--    Each record should display 'tv_shows.title' - 'tv_shows_genres.genre_id'
+--    Results must be sorted in ascending order by 'tv_shows.title' and 'tv_show_genres.genre_id'
+--    If a show doesn’t have a genre, display 'NULL'
+--    You can use only one 'SELECT' statement
+
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
