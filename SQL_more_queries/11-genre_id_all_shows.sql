c17eb7ebe71834ce7eb0185697afb15c168e36e1
tkeeching <tkeeching@gmail.com>
2024-03-27 12:05:43 +1100

    Test

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -7,6 +7,6 @@
 --    You can use only one 'SELECT' statement
 
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
-FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, genre_id;
