e1c53142599754f066a54b69a51e1f2093a58da1
tkeeching <tkeeching@gmail.com>
2024-03-27 11:39:41 +1100

    Test

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,11 +2,11 @@
 --
 -- Requirements:
 --    Each record should display 'tv_shows.title' - 'tv_shows_genres.genre_id'
---    Results must be sorted in ascending order by 'tc_shows.title' and 'tv_show_genres.genre_id'
+--    Results must be sorted in ascending order by 'tv_shows.title' and 'tv_show_genres.genre_id'
 --    If a show doesn’t have a genre, display 'NULL'
 --    You can use only one 'SELECT' statement
 
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, genre_id ASC;
+ORDER BY tv_shows.title, genre_id;
