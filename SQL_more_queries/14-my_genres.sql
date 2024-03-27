2a6efa199c005e9570da8663331b4cb966217d41
tkeeching <tkeeching@gmail.com>
2024-03-27 16:36:21 +1100

    My genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -8,10 +8,10 @@
 --    You can use only one 'SELECT' statement
 
 SELECT
-    tv_genres.name AS genre,
+    tv_genres.name
 FROM
     tv_genres
 JOIN
     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN
-    tv_shows ON tv_shows.name = 'Dexter'
+    tv_shows ON tv_shows.name = 'Dexter';
