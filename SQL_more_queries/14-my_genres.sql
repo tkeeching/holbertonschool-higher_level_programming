dba7cd902d89643001733a897f1c293227d8f8cc
tkeeching <tkeeching@gmail.com>
2024-03-27 16:41:05 +1100

    Test

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -11,7 +11,7 @@ SELECT
     tv_genres.name
 FROM
     tv_genres
-JOIN
+RIGHT JOIN
     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN
     tv_shows ON tv_shows.title = 'Dexter'
