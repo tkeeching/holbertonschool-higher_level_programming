a036923e2f8c8f50229acdb113c675126374b803
tkeeching <tkeeching@gmail.com>
2024-03-27 16:28:16 +1100

    Test

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -14,7 +14,7 @@ SELECT
     COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM
     tv_genres
-LEFT JOIN
+JOIN
     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY 
     tv_genres.name;
