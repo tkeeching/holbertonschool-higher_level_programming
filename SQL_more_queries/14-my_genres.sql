5d9730ce80d4362310cf1964b40700bcee6eea41
tkeeching <tkeeching@gmail.com>
2024-03-27 16:43:50 +1100

    Test

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -10,12 +10,27 @@
 SELECT
     tv_genres.name
 FROM
-    tv_genres
+    tv_shows
+WHERE
+    tv_shows.title = 'Dexter'
 RIGHT JOIN
-    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 RIGHT JOIN
-    tv_shows ON tv_shows.title = 'Dexter'
+    tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY
     tv_genres.name
 ORDER BY
     tv_genres.name;
+
+-- SELECT
+--     tv_genres.name
+-- FROM
+--     tv_genres
+-- RIGHT JOIN
+--     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+-- RIGHT JOIN
+--     tv_shows ON tv_shows.title = 'Dexter'
+-- GROUP BY
+--     tv_genres.name
+-- ORDER BY
+--     tv_genres.name;
