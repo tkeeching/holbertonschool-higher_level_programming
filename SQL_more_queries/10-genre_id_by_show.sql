bc65c6df02eff04528b0b324e905d688a015e8da
tkeeching <tkeeching@gmail.com>
2024-03-27 11:18:14 +1100

    Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- Command to list all shows contained in 'hbtn_0d_tvshows' that have at least one genre linked
+--
+-- Requirements:
+--    Each record should display 'tv_shows.title' - 'tv_shows_genres.genre_id'
+--    Results must be sorted in ascending order by 'tc_shows.title' and 'tv_show_genres.genre_id'
+--    You can use only one 'SELECT' keyword
+
+SELECT tv_shows.title, tv_show_genres.id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.genre_id = tv_show_genres.id
+ORDER BY tv_shows.title, tv_show_genres.id ASC;
