4889868aa0ec75dc79a12c684441a47e26d12371
tkeeching <tkeeching@gmail.com>
2024-03-28 10:00:40 +1100

    Test

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""
-script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
-import sys
+"""Takes in a letter and sends a POST request"""
+
+
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {
-        "q": letter
-    }
+    input = '' if len(sys.argv) == 1 else sys.argv[1]
+    payload = {'q': input}
 
     try:
-        r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-        response = r.json()
-        if response == {}:
-            print("No result")
+        response = requests.post(
+            'http://0.0.0.0:5000/search_user',
+            data=payload)
+
+        json = response.json()
+        if (len(json) == 0):
+            print('No result')
         else:
-            print(f"[{response.get('id')}] {response.get('name')}")
-    except ValueError:
-        print("Not a valid JSON")
+            print('[{}] {}'.format(json['id'], json['name']))
+
+    except IndexError as error:
+        print('IndexError:', error)
+
+    except ValueError as error:
+        print('Not a valid JSON')
