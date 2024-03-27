456ecd33350d7a560e2c5cc9c9e5e275b0556caf
tkeeching <tkeeching@gmail.com>
2024-03-27 12:12:15 +1100

    Test

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -9,4 +9,4 @@
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, genre_id;
+ORDER BY tv_shows.title, genre_id DESC;
