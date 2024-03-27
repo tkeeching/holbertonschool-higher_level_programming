5bbe4cc3cf0d4c7f1f0c8927a147b007aec4e8aa
tkeeching <tkeeching@gmail.com>
2024-03-27 12:03:40 +1100

    Test

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -8,5 +8,5 @@
 
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL');
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, genre_id;
