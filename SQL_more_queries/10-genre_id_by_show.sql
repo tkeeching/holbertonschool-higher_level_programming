853fa5cf4ff0dfe39f6ff9bb5a6be157f18c2420
tkeeching <tkeeching@gmail.com>
2024-03-27 11:28:05 +1100

    Fix requirement

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Requirements:
 --    Each record should display 'tv_shows.title' - 'tv_shows_genres.genre_id'
 --    Results must be sorted in ascending order by 'tc_shows.title' and 'tv_show_genres.genre_id'
---    You can use only one 'SELECT' keyword
+--    You can use only one 'SELECT' statement
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
