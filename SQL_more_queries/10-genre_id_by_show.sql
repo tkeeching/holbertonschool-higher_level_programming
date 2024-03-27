abe547d0d017bbd089cd40aee7303b6028acf161
tkeeching <tkeeching@gmail.com>
2024-03-27 11:20:02 +1100

    Test

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,6 @@
 --    Results must be sorted in ascending order by 'tc_shows.title' and 'tv_show_genres.genre_id'
 --    You can use only one 'SELECT' keyword
 
-SELECT tv_shows.title, tv_show_genres.id FROM tv_shows
-JOIN tv_show_genres ON tv_shows.genre_id = tv_show_genres.id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.genre_id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.id ASC;
