dd84c23390e673fd7424e3baa019c4cf550ddf6b
tkeeching <tkeeching@gmail.com>
2024-03-27 16:57:38 +1100

    Cleanup

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -21,16 +21,3 @@ GROUP BY
     tv_genres.name
 ORDER BY
     tv_genres.name;
-
--- SELECT
---     tv_genres.name
--- FROM
---     tv_genres
--- RIGHT JOIN
---     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
--- RIGHT JOIN
---     tv_shows ON tv_shows.title = 'Dexter'
--- GROUP BY
---     tv_genres.name
--- ORDER BY
---     tv_genres.name;
