fb00cfabd8df310d4765b98f53335995629f9e29
tkeeching <tkeeching@gmail.com>
2024-04-02 09:31:32 +1100

    Case-insensitive

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,7 @@ def main():
     cur.execute("""
         SELECT *
         FROM states
-        WHERE name = '{}'
+        WHERE name = '{}' COLLATE NOCASE
         ORDER BY states.id ASC
     """.format(state_name))
 
