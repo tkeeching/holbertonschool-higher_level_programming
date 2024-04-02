e99b163910e3a801be3f1c638ff5cae27d5ace3f
tkeeching <tkeeching@gmail.com>
2024-04-02 15:36:53 +1100

    Oops

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,7 @@ def main():
     cur = conn.cursor()
 
     cur.execute("""
-        SELECT cities.id, cities.name states.name
+        SELECT cities.id, cities.name, states.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC
