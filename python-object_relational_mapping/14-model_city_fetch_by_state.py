ffc736a95a71e2082b10b8b6e842d637da184319
tkeeching <tkeeching@gmail.com>
2024-04-03 16:43:34 +1100

    Oops

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints all 'State' objects from the database 'hbtn_0e_6_usa'
+Script that prints all 'City' objects from the database 'hbtn_0e_6_usa'
 """
 
 from model_state import State
