9b63625af8cd89ff83531e6adc297b5deb3a73e8
tkeeching <tkeeching@gmail.com>
2024-04-03 16:27:00 +1100

    Update a state

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Script that changes the name of a 'State'
+object from the database 'hbtn_0e_6_usa'
+"""
+
+from model_state import State, Base
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def add_state(username, password, db_name):
+    """Add the State object "Louisiana" to the database"""
+    # Create the engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, db_name))
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query the state object by id
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    # Check if the state object exists
+    if state_to_update:
+        # Update the name attribute
+        state_to_update.name = New Mexico
+
+        # Commit the session to save the changes to the database
+        session.commit()
+    else:
+        print('State not found')
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1:]
+
+    add_state(username, password, db_name)
